9873ad856873947bad224a98422cbf5c4f90d532
Mostafakiz <kzmostafa054@gmial.com>
2024-02-06 18:48:52 +0200

    Welcome

---
+++ database/migrations/2023_12_09_175023_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->string('type')->nullable();
+            $table->string('color');
+            $table->date('date');
+            $table->integer('quantity')->nullable();
+            $table->float('price');
+            $table->json('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
